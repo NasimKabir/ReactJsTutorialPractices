c6f132667801ecc2a0f2f2a419deab9104f59157
NasimKabir <mdkabir334@gmail.com>
2020-04-26 22:39:14 +0600

    updateing form

--- formfields/src/components/FormFields.jsx
+++ formfields/src/components/FormFields.jsx
@@ -2,7 +2,10 @@ import React, { Component } from "react";
 
 class FormFields extends Component {
    state={
-       name:""
+       name:"",
+       email:"",
+       phone:"",
+       city:""
    }
 
 
@@ -10,15 +13,31 @@ class FormFields extends Component {
           this.setState({name:event.target.value})
     }
 
+    submitform=(event)=>{
+        event.preventDefault()
+        console.log(this.state.name)
+    }
     render() {
         return (
             <div className="App">
-                <h1>Hello {this.state.name}</h1>
+                <h1>
+                    {this.state.name}<br></br>
+                    {this.state.email}<br></br>
+                    {this.state.phone}<br></br>
+                    {this.state.city}<br></br>
+                </h1>
                 <div>
-                    <form>
+                    <form onSubmit={this.submitform}>
                         <div>
                             <label>User Name: </label>
                             <input onChange={this.changeFields} type="text" name="username" /><br></br>
+                            <label>email: </label>
+                            <input onChange={this.changeFields} type="email" name="email" /><br></br>
+                            <label>phone: </label>
+                            <input onChange={this.changeFields} type="number" name="phone" /><br></br>
+                            <label>city: </label>
+                            <input onChange={this.changeFields} type="text" name="city" /><br></br>
+
                             <button type="submit" >Submit</button>
                         </div>
                      </form>
